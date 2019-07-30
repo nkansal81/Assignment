d446c2983626259833241d7b3857d7e5b2d0876f
KANSAL <nitika.kansal@thermofisher.com>
2019-07-30 10:22:55 +0530

    first commit

---
+++ Basic_Calculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basic_Calculator
+{
+    class Calculotor
+    {
+        static void Main(string[] args)
+        {
+            Generic_Calculator.Arithematic_operation obj = new Generic_Calculator.Arithematic_operation();
+            dynamic x = obj.Addition(4.4, 5.4);
+            Console.WriteLine("The addition of two number is {0}", x);
+            dynamic y = obj.subtraction(9.5, 8.3);
+            Console.WriteLine("The subtraction of two number is {0}", y);
+
+            dynamic z = obj.Multiplication(4, 2.2);
+            Console.WriteLine("The multiplication two number is {0}", z);
+
+            dynamic i = obj.Division(8.8, 2);
+            Console.WriteLine("The division of two number is {0}", i);
+
+            List<int> obj1 = new List<int>();
+            Console.WriteLine("Enter the number of input");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int j = 1; j <= n; j++)
+            {
+                Console.WriteLine("Enter the input");
+                int num = Convert.ToInt32(Console.ReadLine());
+                obj1.Add(num);
+            }
+            int res = obj.Addition_for_n_input(obj1);
+            Console.WriteLine("The sum of given inputs is {0}", res);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Generic_Calculator/Arithematic_operations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Calculator
+{
+    public class Arithematic_operation
+    {
+        public T Addition<T>(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 + num2;
+        }
+        public T subtraction<T>(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 - num2;
+        }
+        public T Multiplication<T>(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 * num2;
+        }
+
+        public T Division<T>(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            dynamic num3 = 0;
+            if (num2 == 0)
+            {
+                Console.WriteLine("Division Not Posiible");
+                return num3;
+            }
+            else
+            {
+                return num1 / num2;
+            }
+        }
+        public int Addition_for_n_input(List<int> ls)
+        {
+            int sum = 0;
+            foreach (int item in ls)
+            {
+                sum = sum + item;
+            }
+
+            return sum;
+        }
+    }
+
+}
